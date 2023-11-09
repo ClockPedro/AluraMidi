44e32c0a9b84878c81dbc180c26fe98799ae6241
Pedrão Felas <102636895+ClockPedro@users.noreply.github.com>
2023-11-09 10:58:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,41 +1,34 @@
 alert('essa página contém virus. Para continuar forneça seus dados bancários')
 
-function tocaSomPom(){document.querySelector('#som_tecla_pom').play();}
+function tocaSomPom(){document.querySelector('#som_tecla_pom').play()}
 document.querySelector('.tecla_pom').onclck = tocaSomPom;
 
-function tocaSomClap(){document.querySelector('#som_tecla_clap').play();}
+function tocaSomClap(){document.querySelector('#som_tecla_clap').play()}
 document.querySelector('.tecla_clap').onclck = tocaSomClap;
 
-function tocaSomTim(){document.querySelector('#som_tecla_tim').play();}
+function tocaSomTim(){document.querySelector('#som_tecla_tim').play()}
 document.querySelector('.tecla_tim').onclck = tocaSomTim;
 
-function tocaSomPuff(){document.querySelector('#som_tecla_puff').play();}
+function tocaSomPuff(){document.querySelector('#som_tecla_puff').play()}
 document.querySelector('.tecla_puff').onclck = tocaSomPuff;
 
-function tocaSomSplash(){document.querySelector('#som_tecla_splash').play();}
+function tocaSomSplash(){document.querySelector('#som_tecla_splash').play()}
 document.querySelector('.tecla_splash').onclck = tocaSomSplash;
 
-function tocaSomToim(){document.querySelector('#som_tecla_toim').play();}
+function tocaSomToim(){document.querySelector('#som_tecla_toim').play()}
 document.querySelector('.tecla_toim').onclck = tocaSomToim;
 
-function tocaSomPsh(){document.querySelector('#som_tecla_psh').play();}
+function tocaSomPsh(){document.querySelector('#som_tecla_psh').play()}
 document.querySelector('.tecla_psh').onclck = tocaSomPsh;
 
-function tocaSomTic(){document.querySelector('#som_tecla_tic').play();}
+function tocaSomTic(){document.querySelector('#som_tecla_tic').play()}
 document.querySelector('.tecla_tic').onclck = tocaSomTic;
 
-function tocaSomTom(){document.querySelector('#som_tecla_tom').play();}
-document.querySelector('.tecla_Tom').onclck = tocaSomTom;
+function tocaSomTom(){document.querySelector('#som_tecla_tom').play()}
+document.querySelector('.tecla_tom').onclck = tocaSomTom;
 
 const Listadeteclas = document.querySelectorAll('.tecla');
 
-
-document.querySelector('#som_tecla_pom').play
-document.querySelector('.tecla_clap')
-document.querySelector('.tecla_tim')
-document.querySelector('.tecla_puff')
-document.querySelector('.tecla_splash')
-document.querySelector('.tecla_toim')
-document.querySelector('.tecla_psh')
-document.querySelector('.tecla_tic')
-document.querySelector('.tecla_tom')
+Listadeteclas[0].onclick = tocaSomPom while (none) {
+    
+};
